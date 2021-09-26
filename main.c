48c64b7ccee18c8fa7def42e31632d9ee93ad274
gabriele <gabrielejandres@gmail.com>
2021-09-26 13:34:40 -0300

    + modularização

--- main.c
+++ main.c
@@ -14,367 +14,273 @@
 #include "./libs/timer.h"
 #include "./resources/merge_sort.h"
 
-#define NUM_PRODUTORES 1 // numero de threads produtoras
-#define NUM_BLOCOS_BUFFER 10 // numero de blocos do buffer (10 blocos de tamanho N)
+#define NUM_PRODUCERS 1 // numero de threads produtoras
+#define NUM_BUFFER_BLOCKS 10 // numero de blocos do buffer (10 blocos de tamanho N)
 
-/*
- * Sem�foro de consumidores => Exclus�o m�tua entre os consumidores
- * Sem�foro de slot vazio => Sinaliza��o de um slot preenchido para consumo
- * Sem�foro de slot cheio => Sinaliza��o de que todos os slots est�o preenchidos
- * Sem�foro de arquivo livre => Exclus�o m�tua entre os consumidores para a leitura do arquivo, permitindo apenas um por vez.
-*/
+/* Variaveis globais */
+sem_t consumers; // exclusao mutua entre os consumidores
+sem_t empty_slot; // sinalizacao de um slot preenchido para consumo
+sem_t full_slot; // sinalizacao de que todos os slots estao preenchidos
+sem_t free_file; // exclusao mutua entre os consumidores para a escrita no arquivo, permitindo apenas um por vez.
+pthread_mutex_t mutex; // exclusao mutua para a verificacao da necessidade de ler a ultima linha orfa, em caso da presenca da mesma
+int *buffer[10]; // buffer compartilhado entre os arquivos
+int num_consumers; // numero de consumidores solicitado
+int N; // tamanho dos blocos
+int quantity_of_numbers; // quantidade de numeros lida na primeira linha dos arquivos
+int num_rows; // divisao entre a quantidade de numeros e o tamanho dos blocos. Se houver resto, havera uma linha orfa
+char *input_file; // arquivo de entrada
+char *output_file; // arquivo de saida
 
-sem_t consumidores, slot_vazio, slot_cheio, arquivo_livre;
+/* Funcoes */
 
-/* Exclus�o m�tua para a verifica��o da necessidade de ler a �ltima linha �rfa, em caso da presen�a da mesma */
-
-pthread_mutex_t mutex;
-
-/*
- * Buffer (10 X N) => Buffer compartilhado entre os arquivos
- * N�mero de consumidores solicitado
- * N => Tamanho dos blocos
- * Quantidade de n�meros => Lido na primeira linha dos arquivos
- * N�mero de linhas => Divis�o entre a quantidade de n�meros e o tamanho dos blocos. Se houver resto, haver� uma linha �rf�
-*/
-
-int *buffer[10], num_consumidores, N, qtd_numeros, num_linhas;
-
-char *local_arquivo_entrada, *local_arquivo_saida; // Locais dos arquivos
-
-/*
- * Func�o utilizada para o processamento da �ltima linha presente no caso de que o tamanho do bloco
- * n�o divide a quantidade de n�meros. Ou seja, os blocos n�o s�o uniformes.
- * Vetor => espa�o para guardar os valores
- * Arq => Arquivo de sa�da
-*/
-
-// --------------------------------------------------- FUN��ES ---------------------------------------------------
-
-void ordena_linha_orfa(int *vetor, FILE *arq)
+// Funcao utilizada para o processamento da ultima linha presente no caso que a quantidade de numeros nao eh divisivel pelo tamanho do bloco
+void ordenate_last_row(int *array, FILE *output)
 {
-    sem_wait(&slot_cheio); // Aguarda o slot para leitura
+    sem_wait(&full_slot); // aguarda o slot para leitura
 
-    /*
-     * A linha �rf� SEMPRE ser� a �ltima no buffer, pois o arquivo est� sendo lido do in�cio ao fim em ordem e s� h� um produtor.
-     * Logo, ser� exatamente o n�mero de linhas MOD n�mero de buffers.
-    */
+    // A linha orfa SEMPRE sera a ultima no buffer, pois o arquivo esta sendo lido do inicio ao fim em ordem e so ha um produtor. Logo, sera exatamente o numero de linhas MOD numero de buffers.
+    for (int i = 0; i < quantity_of_numbers % N; i++) array[i] = buffer[num_rows % NUM_BUFFER_BLOCKS][i];
 
-    for (int i = 0 ; i < qtd_numeros % N ; i++)
-        vetor[i] = buffer[num_linhas % NUM_BLOCOS_BUFFER][i];
-
-    sem_post(&slot_vazio); // Sinaliza que leu a �ltima linha
-
-    merge_sort(vetor, 0, (qtd_numeros % N) - 1); // Chama o algoritmo de ordena��o
-
-    sem_wait(&arquivo_livre); // Sinaliza que vai utilizar o arquivo
-
-    for (int i = 0 ; i < qtd_numeros % N ; i++)
-        fprintf(arq, "%d ", vetor[i]);
-
-    fprintf(arq,"\n");
-
-    sem_post(&arquivo_livre); // Libera o arquivo
-
+    sem_post(&empty_slot); // sinaliza que leu a ultima linha
+    merge_sort(array, 0, (quantity_of_numbers % N) - 1); // ordenacao
+    sem_wait(&free_file); // sinaliza que vai utilizar o arquivo
+    for (int i = 0; i < quantity_of_numbers % N; i++) fprintf(output, "%d ", array[i]);
+    fprintf(output, "\n");
+    sem_post(&free_file); // libera o arquivo
 }
 
-/*
- * Func�o do produtor
- * Recebe o descritor do arquivo de entrada
-*/
-
-void * le_arquivo(void *arg)
+// Funcao do produtor
+void *read_file(void *file_descriptor)
 {
-    FILE *arq = (FILE *) arg;
+    FILE *input = (FILE *) file_descriptor;
+    int count = 0; // conta quantas linhas UNIFORMES faltam
+    int in = 0; // indica qual slot sera guardado a leitura
 
-    int contador_linhas = 0; // Conta quantas linhas UNIFORMES faltam
-
-    int in = 0; // Indica qual slot ser� guardado a leitura
-
-    while (contador_linhas < num_linhas)
+    while (count < num_rows)
     {
-        sem_wait(&slot_vazio); // Verifica se h� algum slot liberado para produ��o
-
-        for (int i = 0 ; i < N ; i++)
-        {
-            fscanf(arq, "%d", &buffer[in][i]); // Guarda o n�mero no slot atual e na posi��o do vetor
-            printf("%d ", buffer[in][i]);
-        }
-
-        puts("");
-
-        in = (in + 1) % NUM_BLOCOS_BUFFER; // Incrementa o contador do slot
-
-        sem_post(&slot_cheio); // Sinaliza que um consumidor pode utilizar um slot
-
-        contador_linhas++; // Indica que leu uma linha
+        sem_wait(&empty_slot); // verifica se ha algum slot liberado para producao
+        for (int i = 0; i < N; i++) fscanf(input, "%d", &buffer[in][i]); // guarda o numero no slot atual e na posicao do vetor
+        in = (in + 1) % NUM_BUFFER_BLOCKS; // incrementa o contador do slot
+        sem_post(&full_slot); // sinaliza que um consumidor pode utilizar um slot
+        count++; // indica que leu uma linha
     }
 
-    if (qtd_numeros % N != 0) // N n�o � divisor da quantidade de n�meros no arquivo. Haver� uma linha �rf�.
+    if (quantity_of_numbers % N != 0) // N nao eh divisor da quantidade de numeros no arquivo. Havera uma linha orfa.
     {
-        sem_wait(&slot_vazio);
-
-        for (int i = 0 ; i < qtd_numeros % N ; i++)
-        {
-            fscanf(arq, "%d", &buffer[in][i]);
-            printf("%d ", buffer[in][i]);
-        }
-        puts("");
-
-        sem_post(&slot_cheio);
+        sem_wait(&empty_slot);
+        for (int i = 0; i < quantity_of_numbers % N; i++) fscanf(input, "%d", &buffer[in][i]);
+        sem_post(&full_slot);
     }
 
     pthread_exit(NULL);
 }
 
-/*
- * Fun��o concorrente dos consumidores
- * Segue o padr�o de consumidores, com exce��o da �ltima verifica��o
- * No caso de todos os blocos n�o serem uniformes, haver� uma linha �rfa
- * Depois que ela � processada, � marcada uma vari�vel indicando que ela j� foi processada
- * Recebe o descritor do arquivo de sa�da.
-*/
-
-void * ordena_blocos(void *arg)
+// Funcao dos consumidores 
+void *ordenate_blocks(void *file_descriptor)
 {
-    FILE *arq = (FILE *) arg;
-
-    int bloco[N], contador_linhas_thread; // Buffer e contador local da thread
-
-    static int contador_linhas = 0, out = 0; // Contador compartilhado entre as threads e contador do buffer
-
-    static int threads_finalizadas = 0; // Apenas a �ltima thread deve tratar a linha �rfa. Iremos contar quantas j� acabaram.
+    FILE *output = (FILE *) file_descriptor;
+    int block[N], count_rows_thread; // buffer e contador local da thread
+    static int count_rows = 0, out = 0; // contador compartilhado entre as threads e contador do buffer
+    static int finished_threads = 0; // apenas a ultima thread deve tratar a linha orfa. Iremos contar quantas ja acabaram.
 
     pthread_mutex_lock(&mutex);
-
-    /* Se �ltima thread ficar na fila, h� a possibilidade de outras j� terem iniciado.
-     * Essa linha realiza essa captura inicial.*/
-
-    contador_linhas_thread = contador_linhas; 
-
+    count_rows_thread = count_rows; // se a ultima thread ficar na fila, ha a possibilidade de outras ja terem iniciado.
     pthread_mutex_unlock(&mutex);
 
-    while (contador_linhas_thread < num_linhas) // Mesmo racioc�nio da thread produtora
+    while (count_rows_thread < num_rows)
     {
-        /* Verifica se n�o ir� ler linhas a mais */
-
+        // Verifica se nao ira ler linhas a mais
         pthread_mutex_lock(&mutex);
-
-        contador_linhas++;
-
-        contador_linhas_thread = contador_linhas;
-
-        if (contador_linhas > num_linhas)
-        {
-            pthread_mutex_unlock(&mutex);
-            break;
-        }
-
+        count_rows++;
+        count_rows_thread = count_rows;
         pthread_mutex_unlock(&mutex);
+        if (count_rows > num_rows) break;
 
-        /* Padr�o de consumidores */
+        // Padrao de consumidores
+        sem_wait(&full_slot);
+        sem_wait(&consumers);
 
-        sem_wait(&slot_cheio);
+        // toma os blocos do buffer
+        for (int i = 0; i < N; i++) block[i] = buffer[out][i];
+        out = (out + 1) % NUM_BUFFER_BLOCKS;
+        sem_post(&consumers);
+        sem_post(&empty_slot);
+        merge_sort(block, 0, N - 1); // ordenacao
 
-        sem_wait(&consumidores);
-
-        for (int i = 0 ; i < N ; i++)
-            bloco[i] = buffer[out][i];
-
-        out = (out + 1) % NUM_BLOCOS_BUFFER;
-
-        sem_post(&consumidores);
-
-        sem_post(&slot_vazio);
-        merge_sort(bloco, 0, N-1);
-
-        sem_wait(&arquivo_livre); // Bloqueia arquivo para escrita
-
-        for (int i = 0 ; i < N ; i++)
-            fprintf(arq, "%d ", bloco[i]);
-
-        fprintf(arq,"\n");
-
-        sem_post(&arquivo_livre); // Libera arquivo para escrita  
-            
+        // escrita dos blocos ordenados
+        sem_wait(&free_file); // bloqueia arquivo para escrita
+        for (int i = 0; i < N; i++) fprintf(output, "%d ", block[i]);
+        fprintf(output, "\n");
+        sem_post(&free_file); // libera arquivo
     }
 
-    pthread_mutex_lock(&mutex);
-
-    if (threads_finalizadas != num_consumidores - 1) // Se n�o for a �ltima, ent�o deve encerrar normalmente.
+    // Se nao for a ultima thread, entao deve encerrar normalmente.
+    if (finished_threads != num_consumers - 1)
     {
-        threads_finalizadas++;
+        pthread_mutex_lock(&mutex);
+        finished_threads++;
         pthread_mutex_unlock(&mutex);
         pthread_exit(NULL);
     }
-    
-    if (qtd_numeros % N != 0) // Haver� linha �rf� apenas se um n�o for m�ltiplo/divisor do outro
-        ordena_linha_orfa(bloco, arq);
 
-    pthread_exit(NULL);    
+    // Havera linha orfa apenas se um nao for multiplo/divisor do outro
+    if (quantity_of_numbers % N != 0) ordenate_last_row(block, output);
+
+    pthread_exit(NULL);
 }
 
-/* Fun��o unificada para abrir/criar arquivo */
-
-bool toca_arquivo(FILE **arq, const char *local, const char *modo)
+// Funcao para abrir/criar arquivo
+bool handle_file(FILE **file, const char *local, const char *mode)
 {
-    *arq = fopen(local, modo);
-
-    if (!(*arq))
-        return false;
-
+    *file = fopen(local, mode);
+    if (!(*file)) return false;
     return true;
 }
 
-/* Realiza a recep��o e tratamento dos argumentos da linha de comando */
-
-void recebe_argumentos(int argc, char **argv)
+// Funcao para realizar a recepcao e tratamento dos argumentos da linha de comando
+void handle_arguments(int argc, char **argv)
 {
     if (argc < 5)
     {
-        printf("Digite %s <n�mero de consumidores> <tamanho do bloco> <local do arquivo de entrada> <local do arquivo de sa�da>.", argv[0]);
+        printf("Digite %s <numero-de-threads> <tamanho-do-bloco> <arquivo-de-entrada> <arquivo-de-saida>", argv[0]);
         exit(1);
     }
 
-    num_consumidores = atoi(argv[1]);
+    num_consumers = atoi(argv[1]);
     N = atoi(argv[2]);
-    local_arquivo_entrada = argv[3];
-    local_arquivo_saida = argv[4];
+    input_file = argv[3];
+    output_file = argv[4];
 
-    if (num_consumidores <= 0 || N <= 0)
+    if (num_consumers <= 0 || N <= 0)
     {
-        puts("Digite um n�mero de consumidores maior que 0 e um tamanho de bloco maior que 1.");
+        printf("Digite um numero de consumidores e um tamanho de bloco maior que 0 \n");
         exit(2);
     }
 }
 
-/* Aloca��o de espa�o para recursos */
-
-void inicializa_recursos()
+// Funcao para alocar espaco para recursos
+void init()
 {
-    for (int i = 0 ; i < NUM_BLOCOS_BUFFER ; i++)
+    // Alocacao para o buffer compartilhado
+    for (int i = 0; i < NUM_BUFFER_BLOCKS; i++)
     {
         buffer[i] = (int *) calloc(N, sizeof(int));
-
         if (!(buffer[i]))
         {
-            printf("Erro ao alocar espa�o para o buffer %d.\n", i);
-
-            for (int j = 0 ; j < i ; j++)
-                free(buffer[j]);
-
+            printf("Erro ao alocar espaco para o buffer %d \n", i);
+            for (int j = 0; j < i; j++) free(buffer[j]);
             exit(8);
         }
     }
 
-    sem_init(&consumidores, 0, 1); // Apenas um consumidor pode ler o slot
-    sem_init(&slot_vazio, 0, NUM_BLOCOS_BUFFER); // Produtor pode guardar n�meros em todos os slots inicialmente
-    sem_init(&slot_cheio, 0, 0); // N�o h� slot ocupado no come�o. Consumidores ir�o aguardar
-    sem_init(&arquivo_livre, 0, 1); // Seguindo o padr�o leitor/escritor, apenas uma thread pode escrever no arquivo de sa�da
-    pthread_mutex_init(&mutex, NULL); // Exclus�o m�tua para leitura da vari�vel de verifica��o da linha �rf�
+    // Inicializacao dos semaforos e mutex
+    sem_init(&consumers, 0, 1); // apenas um consumidor pode ler o slot
+    sem_init(&empty_slot, 0, NUM_BUFFER_BLOCKS); // produtor pode guardar numeros em todos os slots inicialmente
+    sem_init(&full_slot, 0, 0); // nao ha slot ocupado no comeco. Consumidores irao aguardar
+    sem_init(&free_file, 0, 1); // seguindo o padrao leitor/escritor, apenas uma thread pode escrever no arquivo de saida
+    pthread_mutex_init(&mutex, NULL); // exclusao mutua para leitura da variavel de verificacao da linha orfa
 }
 
-/* Libera��o de recursos alocados no come�o do programa */
-
-void libera_recursos()
+// Funcao para liberar recursos alocados no comeco do programa 
+void destroy()
 {
-    for (int i = 0 ; i < NUM_BLOCOS_BUFFER ; i++)
-        free(buffer[i]);
-
-    sem_destroy(&consumidores);
-    sem_destroy(&slot_vazio);
-    sem_destroy(&slot_cheio);
-    sem_destroy(&arquivo_livre);
+    for (int i = 0; i < NUM_BUFFER_BLOCKS; i++) free(buffer[i]);
+    sem_destroy(&consumers);
+    sem_destroy(&empty_slot);
+    sem_destroy(&full_slot);
+    sem_destroy(&free_file);
     pthread_mutex_destroy(&mutex);
 }
 
-void inicializa_threads()
-{
-    pthread_t threads[num_consumidores + NUM_PRODUTORES];
-    FILE *arq_entrada, *arq_saida;
-
-    if (!toca_arquivo(&arq_entrada, local_arquivo_entrada, "r"))
+// Funcao para abrir os arquivos de entrada e saida 
+void open_files(FILE **input, FILE **output) {
+    if (!handle_file(input, input_file, "r"))
     {
         printf("Erro ao abrir o arquivo de entrada.\n");
         exit(3);
     }
 
-    if (!toca_arquivo(&arq_saida, local_arquivo_saida, "w"))
+    if (!handle_file(output, output_file, "w"))
     {
-        printf("Erro ao criar o arquivo de sa�da.\n");
-        fclose(arq_entrada);
+        printf("Erro ao criar o arquivo de saida.\n");
+        fclose(*input);
         exit(4);
     }
 
-    fscanf(arq_entrada, "%d", &qtd_numeros); // L� a quantidade de n�meros
-
-    /*if (qtd_numeros % 10 != 0)
+    fscanf(*input, "%d", &quantity_of_numbers); // le a quantidade de numeros no arquivo
+    num_rows = quantity_of_numbers / N;
+    if (quantity_of_numbers < N)
     {
-        puts("A quantidade de n�meros no arquivo deve ser um m�ltiplo de 10.");
-        fclose(arq_entrada);
-        fclose(arq_saida);
-        libera_recursos();
-        exit(9);
-    }*/
-
-    num_linhas = qtd_numeros / N;
-
-    if (qtd_numeros < N)
-    {
-        puts("A quantidade de n�meros no arquivo n�o pode ser menor que o tamanho do bloco solicitado.");
-        fclose(arq_entrada);
-        fclose(arq_saida);
-        libera_recursos();
+        printf("A quantidade de numeros no arquivo nao pode ser menor que o tamanho do bloco solicitado \n");
+        fclose(*input);
+        fclose(*output);
+        destroy();
         exit(8);
     }
+}
 
-    if (pthread_create(&threads[0], NULL, le_arquivo, (void *) arq_entrada))
+// Funcao para fechar os arquivos de entrada e saida
+void close_files(FILE *input, FILE *output) {
+    fclose(input);
+    fclose(output);
+}
+// Funcao para aguardar o termino das threads
+void join_threads(pthread_t *threads, FILE *input, FILE *output, int num_threads) {
+    for (int i = 0; i < num_threads; i++)
     {
-        puts("Erro ao criar a thread produtora.");
-        fclose(arq_entrada);
-        fclose(arq_saida);
-        libera_recursos();
-        exit(5);
-    }
-
-    for (int i = 1 ; i <= num_consumidores ; i++)
-    {
-        if (pthread_create(&threads[i], NULL, ordena_blocos, (void *) arq_saida))
-        {
-            printf("Erro ao criar uma thread consumidora. ID: %d.\n", i);
-            fclose(arq_entrada);
-            fclose(arq_saida);
-            libera_recursos();
-            exit(6);
-        }
-    }
-
-    for (int i = 0 ; i < num_consumidores + NUM_PRODUTORES ; i++)
-    {
-        if (pthread_join(threads[i], NULL))
+        if (pthread_join(*(threads + i), NULL))
         {
             printf("Erro ao aguardar a thread %d.\n", i);
-            fclose(arq_entrada);
-            fclose(arq_saida);
-            libera_recursos();
+            close_files(input, output);
+            destroy();
             exit(7);
         }
     }
 
-    fclose(arq_entrada);
-    fclose(arq_saida);
+    close_files(input, output);
 }
 
+// Funcao para criar as threads
+void create_threads(pthread_t **threads, int consumers, int producers, FILE *input, FILE *output)
+{
+    *threads = (pthread_t *) malloc(sizeof(pthread_t) * (consumers + producers));
+    if (*threads == NULL) {
+        printf("Erro na alocacao de memoria para threads \n");
+        exit(2);
+    }
+
+    // cria thread produtora
+    if (pthread_create((*threads), NULL, read_file, (void *)input))
+    {
+        printf("Erro ao criar a thread produtora \n");
+        close_files(input, output);
+        destroy();
+        exit(5);
+    }
+
+    // cria threads consumidoras
+    for (int i = 1; i <= num_consumers; i++)
+    {
+        if (pthread_create((*threads) + i, NULL, ordenate_blocks, (void *)output))
+        {
+            printf("Erro ao criar uma thread consumidora. ID: %d.\n", i);
+            close_files(input, output);
+            destroy();
+            exit(6);
+        }
+    }
+}
 
 int main(int argc, char **argv)
 {
-    recebe_argumentos(argc, argv);
+    FILE *input, *output; // arquivos de entrada e saida
+    pthread_t *threads; // identificadores das threads
 
-    inicializa_recursos();
-
-    inicializa_threads();
-
-    libera_recursos();
-
+    handle_arguments(argc, argv);
+    open_files(&input, &output);
+    init();
+    create_threads(&threads, num_consumers, NUM_PRODUCERS, input, output);
+    join_threads(threads, input, output, num_consumers + NUM_PRODUCERS);
+    destroy();
     return 0;
 }
